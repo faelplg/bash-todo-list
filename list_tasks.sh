bba4b595da2a8bcf2d6f2d2197e1eb68dc51f18e
Fael <faelplg@gmail.com>
2025-06-29 13:09:48 -0300

    improving list tasks and adding position column.

--- list_tasks.sh
+++ list_tasks.sh
@@ -2,6 +2,13 @@
 
 TASK_FILE="tasks.txt"
 
+# Define color variables
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+YELLOW='\033[0;33m'
+BLUE='\033[0;34m'
+NC='\033[0m' # No Color
+
 # Ensures that the script only proceeds if the $TASK_FILE exists
 # and contains data. If the file is missing or empty, it gracefully
 # exits after notifying the user.
@@ -11,16 +18,16 @@ if [[ ! -f "$TASK_FILE" || ! -s "$TASK_FILE" ]]; then
 fi
 
 # Displays the tasks in a table format
-echo "Tasks list:"
-echo "--------------------------------------------------------------------------------"
-printf "%-40s | %-20s | %-10s\n" "Tarefa" "Projeto" "Status"
-echo "--------------------------------------------------------------------------------"
-while IFS="|" read -r task_name project_name task_status; do
+echo "TASK BOARD"
+echo "------------------------------------------------------------------------------------------"
+printf "%-5s | %-40s | %-20s | %-10s\n" "Pos" "Task" "Project" "Status"
+echo "------------------------------------------------------------------------------------------"
+while IFS="|" read -r pos task_name project_name task_status; do
 	# Trunca o nome da tarefa se for maior que 30 caracteres
 	if [[ ${#task_name} -gt 40 ]]; then
 		task_name="${task_name:0:37}..."
 	fi
-	printf "%-40s | %-20s | %-10s\n" "$task_name" "$project_name" "$task_status"
+	printf "%-5s | ${YELLOW}%-40s${NC} | %-20s | %-10s\n" "$pos" "$task_name" "$project_name" "$task_status"
 done < "$TASK_FILE"
 
-echo "--------------------------------------------------------------------------------"
+echo "------------------------------------------------------------------------------------------"
