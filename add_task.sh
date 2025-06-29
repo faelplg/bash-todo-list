a91ad3a32b8f04cdaf22a495207f7b83745b0209
Fael <faelplg@gmail.com>
2025-06-29 01:30:25 -0300

    adding real tasks for this project

--- add_task.sh
+++ add_task.sh
@@ -2,17 +2,24 @@
 
 TASK_FILE="tasks.txt"
 
+# Define color variables
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+YELLOW='\033[0;33m'
+BLUE='\033[0;34m'
+NC='\033[0m' # No Color
+
 # Requests new task data
-echo "Enter task name: "
+echo -e "\n${BLUE}Enter task name: ${NC}"
 read task_name
 
-echo "Enter project name: "
+echo  -e "\n${BLUE}Enter project name: ${NC}"
 read project_name
 
-echo "Enter task status: "
+echo  -e "\n${BLUE}Enter task status: ${NC}"
 read task_status
 
 # Add task in the file
 echo "$task_name|$project_name|$task_status" >> "$TASK_FILE"
 
-echo "Task added successfully."
+echo -e "\n${GREEN}Task added successfully.${NC}"
